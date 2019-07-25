173ff44c1d6f0f1b4169b73e7e0c60891e4fe7de
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-07-25 00:20:43 +0000

    updated

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,4 +1,13 @@
-const input = process.argv.slice(2)
+// const input = process.argv.slice(2)
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+// const values = [`%${cohortName}%`, limit];
+
+const values = [`%${cohortName}%`, limit];
+
+console.log(values)
 
 const { Pool } = require('pg');
 
@@ -9,15 +18,20 @@ const pool = new Pool({
   password: 'vagrant'
 });
 
+
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-where cohorts.name like '%${input[0]}%'
-LIMIT ${input[1]};
-`)
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name like $1
+  LIMIT $2;
+`, values)
 .then(res => {
+  // console.log('check1')
   res.rows.forEach(user => {
+    //console.log('check3')
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-});
+  // console.log('check2')
+})
+.catch(err => {console.log('Failed')});
